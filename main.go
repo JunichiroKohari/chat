d35896b30db60b903923850fd71326efe07ed348
JunichiroKohari <junichiro@kohari.jp>
2023-12-31 17:36:49 +0900

    Feat: add html template

--- main.go
+++ main.go
@@ -3,21 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>chat</title>
-				</head>
-				<body>
-					hello
-				</body>
-			</html>
-		`))
+type template1Handler struct {
+	once      sync.Once
+	filename  string
+	template1 *template.Template
+}
+
+func (t *template1Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.template1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.template1.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &template1Handler{filename: "chat.html"})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
