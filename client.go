d2c9bc995129cc60a17c0078dd8025525c2da045
JunichiroKohari <junichiro@kohari.jp>
2024-01-01 20:40:54 +0900

    Feat: add chat ui and handling request

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type client struct {
 }
 
 func (c *client) read() {
+	defer c.socket.Close()
 	for {
 		if _, msg, err := c.socket.ReadMessage(); err == nil {
 			c.room.forward <- msg
@@ -18,14 +19,13 @@ func (c *client) read() {
 			break
 		}
 	}
-	c.socket.Close()
 }
 
 func (c *client) write() {
+	defer c.socket.Close()
 	for msg := range c.send {
 		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
 			break
 		}
 	}
-	c.socket.Close()
 }
