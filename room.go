a22fca67b2ca710b38041dce75f14a9acb39600d
JunichiroKohari <junichiro@kohari.jp>
2024-01-01 09:54:23 +0900

    Feat: add room model

---
+++ room.go
@@ -0,0 +1,31 @@
+package main
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// send messsage
+				default:
+					// fail to send message
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
