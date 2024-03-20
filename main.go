2dd1fc6390868289c2fb82b79371f82d0990d1f0
JunichiroKohari <junichiro@kohari.jp>
2024-03-20 20:21:58 +0900

    Add login page

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 	"text/template"
 )
 
-type template1Handler struct {
+type templateHandler struct {
 	once      sync.Once
 	filename  string
 	template1 *template.Template
 }
 
-func (t *template1Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.template1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
@@ -27,7 +27,8 @@ func main() {
 	flag.Parse()
 	r := newRoom()
 	// r.tracer = go_trace.New(os.Stdout)
-	http.Handle("/chat", MustAuth(&template1Handler{filename: "chat.html"}))
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 	go r.run()
 	log.Println("Start Web Server. Port: ", *addr)
